c16d913c64f739edf38bf1fe4b824e3e714d53a7
lisa <fafrin613@gmail.com>
2021-05-27 00:55:55 +0600

    have done url mapping for login

--- adminpanel/user/urls.py
+++ adminpanel/user/urls.py
@@ -2,10 +2,10 @@ from django.urls import *
 from . import views
 
 urlpatterns=[
-	
+
     path('',views.loginview,name="loginview"),
-    path('signup/',views.signup,name="signup")
-    
-    
+    path('signup/',views.signup,name="signup"),
+	path('login/',views.login,name="login")
+
 
 ]
