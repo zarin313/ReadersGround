93d5f6282b5bf25c8d7b2f2429a7b94056998bea
zarin313 <zbhuiyan171120@bscse.uiu.ac.bd>
2021-06-05 11:25:40 +0600

    sql

--- readers_hub.sql
+++ readers_hub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2021 at 11:25 PM
+-- Generation Time: Jun 04, 2021 at 06:39 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -134,6 +134,37 @@ CREATE TABLE `auth_user_user_permissions` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `user` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `pid`, `content`, `user`) VALUES
+(39, 8, 'this one is my favorite', 'zaima6'),
+(42, 9, 'hey', 'oroni12'),
+(46, 11, 'wow', 'oroni12'),
+(47, 10, 'nice one', 'oroni12'),
+(48, 8, 'hey', 'oroni12'),
+(54, 9, 'wow', 'zaima6'),
+(55, 12, 'good one', 'oroni12'),
+(56, 9, 'good', 'oroni12'),
+(57, 12, 'good', 'zarin36'),
+(58, 8, 'nice', 'nawaf14'),
+(59, 11, 'devastating', 'happy23'),
+(60, 11, 'best', 'oroni12');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `django_admin_log`
 --
 
@@ -226,7 +257,26 @@ CREATE TABLE `django_session` (
 --
 
 INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
-('5u2dazpuo23fjnicy25wvqmpd0t8j745', 'eyJ1c2VybmFtZSI6Im9yb25pMTIifQ:1llHvb:4ZbA5qbgM0mNvZjeuojG4vL6YUaSsCxYjgAdSNR1V3o', '2021-06-07 21:15:59.234019');
+('06rmv17zyx9szji43uaym0oodkphxmuc', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lowG3:l1XOsCtajW1oEvul5zeg-mjA4C4w7I067wu_ioCnh5I', '2021-06-17 22:56:11.731765'),
+('23kjxdnbtujigd5rotl72li3tf223pln', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpBdC:-3Ck6OG3l6RDu0yc0rF_E2-HnY5-xOoMh7QtuQaMDo0', '2021-06-18 15:21:06.032773'),
+('44lfbl5r32syeviawn5euc1jut6keqqh', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low1Y:Ql11DMVwRyAfegcDLfd1Wa48CqXWY9A8jaN9-1gIuJM', '2021-06-17 22:41:12.126833'),
+('4r83vxftlybodj9gaa9qpu8a4lin8w7e', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpCBC:yafYWJcQicsVflfe04asQce1SsKd_yuR94wA3mzl_DQ', '2021-06-18 15:56:14.036031'),
+('5jbuvf67xwri506do8emrppjbbaki065', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpBef:p8zIDn5CAX-OhQJl0hg8nsDL9HILt-Zoi4qBUywau2Q', '2021-06-18 15:22:37.300877'),
+('7dvbm43dmw20tic3r65a78mz6l0z70aq', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low4C:_tVVvR6IkpE5xyoVxOB181G2qrhO0mIyusP4GFkkYXQ', '2021-06-17 22:43:56.882118'),
+('9abdovhkg3rqpcm01510m508uh1k3jui', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low62:zt-pyI0mowjzMX60fJzBBibb-Inbcnl-50GjOMzoasQ', '2021-06-17 22:45:50.352645'),
+('bu8d6hscj65l0bhg95aksnrj0j8o5q6s', 'e30:1lpC49:D7EBSoWZCu4v3Ye0oiaHYa6Y3hbh9crW-VU232Dpw34', '2021-06-18 15:48:57.971162'),
+('f2fmrjmgg9yr8ghb98iob79yqiz5isa5', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low9h:ODqNUfMfDalnB9EjbpDF6ZmUQ13yz8OP_nsrOCom5dM', '2021-06-17 22:49:37.180353'),
+('f6o21cojxl4brv7ua77382otqbbvfvye', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low90:i42GbeLN1Ag5FG_pcs7QOgNEifMP4BKNhFMc-v57Efw', '2021-06-17 22:48:54.299781'),
+('flspe3n73vafpys3uh5s20wuh7do53lf', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lowBh:oENF5KP7iu4ft61bzGcv2T7HgBRl3BwxFcUY5rpooJw', '2021-06-17 22:51:41.761289'),
+('ftgcd2tbhvqnfp0sgd8expji80so3jpo', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpCIg:Jjx2iDm4N0nG_Qgaidtaz_UuNQFrvNk6z4vgaiPGcYw', '2021-06-18 16:03:58.495627'),
+('htxu2w66au5d868yx2j64ql62z82zphy', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lowCk:8irtywWhgnZlDqaOf_oUyjKkfqBeQc9WKRQiq7GnEL8', '2021-06-17 22:52:46.738901'),
+('igmoftv80zkklpxxxgzcmoqxjgnfhw5v', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lovvV:b8vX_zEXrr6JbwbDUTQUBwoH-inT2nTv9tBfP1GlM0s', '2021-06-17 22:34:57.661585'),
+('kfn81k59b40ak8mf5x3pbpibgsy9a7uz', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpBff:L5eSkJkDpy2qEparmsHwX35Flld_HIM1yostxHuS8CI', '2021-06-18 15:23:39.940573'),
+('tahntk0lh8xb0rvy4gcijptqwluditmp', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpBaD:PgxI8gIu2bSI45NeKSHUrqFUuQlC7m0ZWx0wck5q0Ww', '2021-06-18 15:18:01.995776'),
+('wqzvtaxgifa3r0ikecdzx262ch7w4cq7', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lowEJ:xKHYMBFUf4VFrvfNEfx2lwCReH2XCd2KWnDwD8jLA_s', '2021-06-17 22:54:23.618804'),
+('wyxal8f4k85f6jele63atzm8efkx45sf', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1lpCMN:5y3BsB8o1irbPcA1oi0oWiYKFvCj2WJMp5nYcDPgPsU', '2021-06-18 16:07:47.720797'),
+('x0n3xhxqdm606m81oujqf6c4j5wutea3', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low3O:oSIy2x1DFhkiFAKZrCaYaaaCltjeuEZHak-f3T6ksaA', '2021-06-17 22:43:06.257412'),
+('z0dyzj21zzh1bu88q3v768w9dn840nca', 'eyJ1c2VybmFtZSI6InphaW1hNiJ9:1low7r:y0kwUB39L4o6w-p08hNqJWZ9O3F05Ky5qJcDeqNOa34', '2021-06-17 22:47:43.667754');
 
 -- --------------------------------------------------------
 
@@ -237,6 +287,7 @@ INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALU
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `uname` varchar(50) NOT NULL,
+  `bname` varchar(40) DEFAULT NULL,
   `writer` varchar(50) NOT NULL,
   `genre` varchar(50) NOT NULL,
   `rate` varchar(50) NOT NULL,
@@ -249,8 +300,12 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `uname`, `writer`, `genre`, `rate`, `review`, `upldate`, `imgpath`) VALUES
-(0, 'df z', 'df', 'Action and Adventure', 'Star :D', 'zcv', '2021-05-24', '/media/IMAG0126.jpg');
+INSERT INTO `product` (`id`, `uname`, `bname`, `writer`, `genre`, `rate`, `review`, `upldate`, `imgpath`) VALUES
+(8, 'oroni12', '', 'Humayun Ahmed', 'Action and Adventure', 'Average :)', 'its a really good book. Humayun Ahmed is always my favorite. most of his books don\'t have a prefect ending. but it has. thats why I loved it more and mre', '2021-06-04', '/media/Aj%20ami%20kothao%20jabo%20na%20by%20Humayun%20Ahmed.jpg'),
+(9, 'oroni12', 'Salvation of Saint', 'Keigo Higashino', 'Detective and Mystery', 'Star :D', 'One of the detective Galleleo books. its quite thrilling. You never know what comes next. one of the best book I have ever read.', '2021-06-04', '/media/Screenshot%202021-06-04%20022302.png'),
+(10, 'oroni12', 'Devotion of Suspect X', 'Keigo Higashino', 'Detective and Mystery', 'Star :D', 'I have become fan of Keigo Higashino after reading two of his books. Best thriller story it is. though I found the ending quite dramatic.', '2021-06-04', '/media/Screenshot%202021-06-04%20022338.png'),
+(11, 'zaima6', 'The Kite Runner', 'Khaled Hossaini', 'Action and Adventure', 'Star :D', 'A gripping and emotional story of betrayal and redemption, The Kite Runner had me thrilled and moved, both at the same time. It tells the story of Amir and Hassan, the closest of friends, as good as brothers, and also experts in the art of kite flying. The two young boys live in Kabul, the capital of Afghanistan, and this year they are going to try harder than ever to win the local kite-fighting tournament—a popular Afghan pastime, and this is Amir\'s one hope of winning his father\'s love.', '2021-06-04', '/media/Screenshot%202021-06-04%20022902.png'),
+(12, 'zaima6', 'Harry Potter and the prisoner of Azkaban', 'J K Rowling', 'Fantasy', 'Star :D', 'My favorite among all the Harry Potter books. I shows the value of friendship. It shows how a close person can betray without giving any clue.', '2021-06-04', '/media/Screenshot%202021-06-04%20022952.png');
 
 -- --------------------------------------------------------
 
@@ -271,12 +326,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`uname`, `name`, `email`, `password`, `reviews`) VALUES
-('fvafvva', 'vfsdzv', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$2bwExeuOU5w0M3ptDLWsrV$15bHfnHLCFTVmJsnLea1ZxSPE1E8IlwTO+e1ag1Ab5I=', 0),
+('happy23', 'happy', 'zaima.tasmia@gmail.com', 'pbkdf2_sha256$260000$LxDnTM5ZQ1uq7dP9phG8rc$riP1qLPaPQs5UYuJI/xqFGy2/llTrY3BAbi2pF8sh14=', 0),
+('nawaf14', 'nawaf14', 'zaima.tasmia@gmail.com', 'pbkdf2_sha256$260000$VFGUyS3uQO10bIrTpMTkoZ$6PR2SEvI30bfwtHKbGnqJhHRhYhLvocDBH8r/EpN9HY=', 0),
+('oroni1', 'oroni1', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$2Ni2yMBrztGu3seGWxM7e0$fcCjysKHkhB0qLemao34iAywDmHezdTpJxrmSmrjtw4=', 0),
 ('oroni12', 'zarin oroni', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$sTCFJ17WBV0tbtPtAMbzpl$TFHzlx5g2HXuWvjxlFl5Mo8bhTUEe3POVuxCvBYOomI=', 0),
-('oroni3', 'oro', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$5OESHr01dOKs6awuVHnU9D$YRoj+UDXMMVyuSQ7k/qzP6QtrEWIN+clrVCFSCdA7iQ=', 0),
-('sdsdvsdvcs', 'oro', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$CBHPNoQOyOLlyJEQPhNNEA$N9OoMl8N/u1zuIj2AdI07sUOSu3sV6trKKavq2DbT38=', 0),
-('svdavdwf', 'dvzdfdzd', 'favf', 'pbkdf2_sha256$260000$frXqB4t8gpSxp3riYZqd9K$Btox533QNTkT7vLoP8lp9CBnrDE9V+dU1pRat+pm07E=', 0),
-('svfdfv1', 'sdvav', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$5aQQlWGROSlYoz4IXT1g7i$HGDVsp/IsqjFFuisWM73+ynAgAy+J1OAoESOuDQGVrI=', 0);
+('zaima6', 'zaima', 'zarinraiyanoroni@gmail.com', 'pbkdf2_sha256$260000$mF2jxTTns0BZZK3bkO6nTU$wb8iNN5uyrb99XRs7Mud2PN9QlOuwfeZ+5nmNyZDA8c=', 0),
+('zarin36', 'oroni', 'zaima.tasmia@gmail.com', 'pbkdf2_sha256$260000$Vhr49yaK4SwnIOBwEWNw0n$foj2lvI+tFbATU9Fu28kXipL+YwNMxOPRNfY5L9YvXE=', 0);
 
 --
 -- Indexes for dumped tables
@@ -326,6 +381,12 @@ ALTER TABLE `auth_user_user_permissions`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
   ADD KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `django_admin_log`
@@ -408,6 +469,12 @@ ALTER TABLE `auth_user_user_permissions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
 -- AUTO_INCREMENT for table `django_admin_log`
 --
 ALTER TABLE `django_admin_log`
@@ -424,6 +491,12 @@ ALTER TABLE `django_content_type`
 --
 ALTER TABLE `django_migrations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
